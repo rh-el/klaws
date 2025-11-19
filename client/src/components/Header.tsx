aa6dd91405f646a952fa111bdf91dfe3d4b9846a
William Petitpierre <william.petitpierre@dailymotion.com>
2025-11-19 23:19:20 +0100

    updated header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Avatar, Tabs, View } from "reshaped";
 
 export default function Header() {
 	return (
-		<div className="flex justify-between max-w-[1352px]">
+		<div className="box-border max-w-[1352px] flex justify-between">
 			<Avatar src="" initials="K" color="primary" variant="faded" size={12} />
 			<Tabs variant="borderless" defaultValue="1">
 				<Tabs.List>
@@ -12,8 +12,4 @@ export default function Header() {
 			</Tabs>
 		</div>
 	);
-}
-//
-{
-	/* </div> */
 }
