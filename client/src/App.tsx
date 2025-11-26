dee9652549162fdbf2c1c76475f1dbaf9ef47acb
William Petitpierre <william.petitpierre@dailymotion.com>
2025-11-26 19:08:43 +0100

    updated reshaped version package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,19 @@
 import { Avatar, Container, Tabs, View } from "reshaped";
 
 function App() {
-  return (
-    <Container width="1280px">
-      <View direction="row" align="center" justify="space-between" gap={6} width="100%">
-        <Avatar src="" initials="K" color="primary" variant="faded" size={12} />
-
-        <Tabs variant="borderless" defaultValue="1">
-          <Tabs.List>
-            <Tabs.Item value="1">worKs</Tabs.Item>
-            <Tabs.Item value="2">aKount</Tabs.Item>
-          </Tabs.List>
-        </Tabs>
-      </View>
-    </Container>
-  );
+	return (
+		<Container width="1280px">
+			<View direction={"row"} justify={"space-between"} className="flex-nowrap w-full">
+				<Avatar src="" initials="K" color="primary" variant="faded" size={12} />
+				<Tabs variant="borderless" defaultValue="1">
+					<Tabs.List className="w-fit">
+						<Tabs.Item value="1">worKs</Tabs.Item>
+						<Tabs.Item value="2">aKount</Tabs.Item>
+					</Tabs.List>
+				</Tabs>
+			</View>
+		</Container>
+	);
 }
 
 export default App;
-
-<Container width="1280px">
-  <View direction="row" align="center" gap={6} width="100%">
-    <Avatar src="" initials="K" color="primary" variant="faded" size={12} />
-
-    <View direction="row" justify="end" grow>
-      <Tabs variant="borderless" defaultValue="1">
-        <Tabs.List>
-          <Tabs.Item value="1">worKs</Tabs.Item>
-          <Tabs.Item value="2">aKount</Tabs.Item>
-        </Tabs.List>
-      </Tabs>
-    </View>
-  </View>
-</Container>;
